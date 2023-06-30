0b8a931d0196e47989c1bbcd48c751947f4cf92d
JASaddington <jasaddington@gmail.com>
2023-06-30 16:51:44 -0500

    setting up custom components and styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Chat from "@/components/chat";
 
 function App() {
 
@@ -10,7 +11,6 @@ function App() {
           <Route path="/chat" element={<Chat />} />
         </Routes>
       </BrowserRouter>
-      app
     </div>
   )
 }
