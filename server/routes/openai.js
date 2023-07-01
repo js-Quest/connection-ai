9cd4dd34ebea5ed58d82d43a4ae1b03d16806ed4
JASaddington <jasaddington@gmail.com>
2023-07-01 16:02:59 -0500

    starting server/routes/openAi.js, successful response from openAI API call

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -10,8 +10,31 @@ const router = express.Router();
 router.post("/text", async (req,res) => {
   try{
     const { text, activeChatId } = req.body;
-    console.log("🚀 ~ file: openai.js:13 ~ router.post ~ text:", text)
-    res.status(200).json({ text })
+    
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: text,
+      temperature: 0.5, //between 0-2, lower values are more strict; 0.8 would be more random.
+      max_tokens: 2048,
+      top_p: 1,
+      frequency_penalty: 0.5, // -2 to 2.  Penalty for repeating same line.
+      presence_penalty: 0 // -2 to 2, positive values increase model's likelihood of new topics.
+    })
+
+    //basically making call to handlesubmit
+    await axios.post(
+      `https://api.chatengine.io/chats/${activeChatId}/messages/`,
+      { text: response.data.choices[0].text }, //grab text of response
+      {
+        headers: {
+          "Project-ID": process.env.PROJECT_ID,
+          "User-Name": process.env.BOT_USER_NAME,
+          "User-Secret": process.env.BOT_USER_SECRET,
+        }
+      }
+    )
+
+    res.status(200).json({ text: response.data.choices[0].text })
   }catch(error){
     console.error("error", error)
     res.status(500).json({error: error.message})
